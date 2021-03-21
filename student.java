720fa01b5248e142fb4faa7ed723a119267f1df8
Neelabh Paul <neelabh0101@gmail.com>
2021-03-21 23:37:59 +0530

    First upload

---
+++ student.java
@@ -0,0 +1,28 @@
+import java.util.*; 
+
+public class student 
+{
+	String info = "";
+	List<String> dlist = new ArrayList<String>();
+	List<String> output = new ArrayList<String>();
+	String ddt = "";
+	String date = "";
+	String lines = "";
+	String sbdt1 = "";
+	
+	
+	void getval(String a, String b, String c)
+	{
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
